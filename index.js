8ff6490fb3b78c143cccbfdc7a592e644599fdaf
ryanyychen <ryan_yychen@hotmail.com>
2023-04-22 14:24:37 -0700

    Added details to webpage

--- index.js
+++ index.js
@@ -1,54 +1,74 @@
 var courses = new Map();
+courses.set('MATH 181A', [null]);
+courses.set('CSE 20', ['CSE 11']);
+courses.set('CSE 21', ['CSE 20']);
+courses.set('CSE 100', ['CSE 21', 'CSE 30']);
+courses.set('CSE 101', ['CSE 21']);
+courses.set('CSE 105', ['CSE 12', 'CSE 15L', 'CSE 20', 'CSE 21']);
+courses.set('CSE 110', ['CSE 100']);
+courses.set('MATH 20A', [null]);
+courses.set('MATH 20B', ['MATH 20A']);
+courses.set('MATH 20C', ['MATH 20B']);
 courses.set('CSE 11', [null]);
 courses.set('CSE 12', ['CSE 11']);
 courses.set('CSE 15L', ['CSE 11']);
 courses.set('CSE 30', ['CSE 12', 'CSE 15L']);
 
-var completed = [null];
-var layerNum = 1;  
-/* need some determination for when to stop a while loop for constructing nodes */
-while (layerNum < 4) {
-   console.log(completed);
+/* Sort map in alphabetical order */
+courses = new Map([...courses.entries()].sort());
+
+var completed = [];
+var layerNum = 1;
+var courseList = Array.from(courses.keys());
+
+/* Run while requirements for degree not completed */
+while (true) {
+   let graduated = true;
+   for (let i = 0; i < completed.length; i++) {
+      if (!completed.includes(courseList[i])) {
+         graduated = false;
+      }
+   }
+   if (completed.length != courseList.length) graduated = false;
+   if (graduated) break;
    /* Use iterator to iterate map */
-   var iterator = courses.keys();
+   let iterator = courses.keys();
 
    /* Create new layer to tree */
-   var layer = document.createElement('div');
+   let layer = document.createElement('div');
    layer.className = "tree_layer";
    layer.id = "layer" + layerNum;
    /* Append layer to tree */
-   var parentDiv = document.getElementById("tree_display");
+   let parentDiv = document.getElementById("tree_display");
    parentDiv.appendChild(layer);
-   console.log("Added layer");
 
-   var taking = [];
+   let taking = [];
 
    /* Iterate map to find classes that can be taken
       and create nodes of those classes */
    while (true) {
-      console.log(taking);
-      var courseName = iterator.next().value; /* courseName = (key, done: T/F) */
+      let courseName = iterator.next().value; /* courseName = (key, done: T/F) */
       if (courseName == null) break;
-      console.log("Course: " + courseName);
 
       /* If node of course does not already exist then check for prerequisites
          then create new node if all prereqs satisfied*/
       if (!completed.includes(courseName)) {
-         console.log("Attempting to add: " + courseName);
          /* Get array of prerequisites using key */
-         var prerequisites = courses.get(courseName);
-         var canTake = true;
+         let prerequisites = courses.get(courseName);
+         let canTake = true;
          /* Iterate through each prerequisite and check if in 'completed' array */
-         for (let i = 0; i < prerequisites.length; i++) {
-            if (!completed.includes(prerequisites[i])) {
-               canTake = false;
+         if (prerequisites.length != 1 || prerequisites[0] != null) {
+            for (let i = 0; i < prerequisites.length; i++) {
+               if (!completed.includes(prerequisites[i])) {
+                  canTake = false;
+               }
             }
          }
          /* If all prereqs satisfied, create new node and add to layer */
          if (canTake) {
-            var node = document.createElement('div');
+            let node = document.createElement('div');
             node.className = "node";
-            var course = document.createElement('span');
+            let course = document.createElement('span');
             course.className = "course";
             course.id = courseName;
             course.textContent = courseName;
