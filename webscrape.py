8cb1255e8e4b938732f6b2d362ed264ced801d8e
unknown <bkreponte@ucsd.edu>
2023-04-22 14:54:59 -0700

    finished webscrape.py for CSE department

--- webscrape.py
+++ webscrape.py
@@ -1,8 +1,9 @@
 # Web scraping script
 
 # imports
-import requests
 from bs4 import BeautifulSoup
+import requests
+import json
 
 def getHTMLContent(url):
     """ 
@@ -16,9 +17,10 @@ def getHTMLContent(url):
     """
     site = requests.get(url)
     htmlContent = BeautifulSoup(site.content, 'html.parser')
+
     return htmlContent
 
-def populateDict(htmlContent):
+def getCourseCatalog(htmlContent):
     """
     Fill a dictionary with keys = courses, values = array of prerequisites
 
@@ -29,18 +31,61 @@ def populateDict(htmlContent):
         dict: keys = courses, values = array of prerequisites
     """
     courseCatalog = dict()
+    # get a list of course titles
+    courseTitles = htmlContent.find_all('p', {'class': 'course-name'})
+    # call getPrereqs() to populate each course with its prerequisites
+    for course in courseTitles:
+        # get course description of given course
+        courseDesc = course.find_next_sibling()
+        # find "Prerequisites:", that begins list of prerequisites
+        preq_tag = courseDesc.find_next(['em', 'span', 'strong'])
+        prerequisitesText = None
+        if type(preq_tag) == type(None):
+            prerequisites = []
+        else:
+            # store list of prerequisites
+            prerequisitesText = preq_tag.next_sibling.strip()
+            prerequisites = getPrereqs(prerequisitesText)
+        # removes units because unicode incompability
+        noUnit = course.text.index(" (")
+        # inserts course and its prerequisites into courseCatalog
+        courseCatalog[course.text[:noUnit]] = prerequisites
     return courseCatalog
 
-def getPrereqs(courseTitle):
+def getPrereqs(prerequisitesText):
     """
-    Get the prerequisites of the given course
+    Get the prerequisites of the given course by parsing prerequisitesText
 
     Args:
-        courseTitle (string): course title scraped from getHTMLContent() and 
-        populateDict()
+        prerequisitesText (string): prerequisites list scraped from 
+        getHTMLContent() and getCourseCatalog()
 
     Returns:
         string: array of prerequisites
     """
-    prerequisites = []
-    return prerequisites
+    prerequisitesArr = []
+
+    # get first word of list, if it is not a valid course id then
+    # return empty list as no prerequisites exist.
+    prerequisitesText = str(prerequisitesText)
+    first_word = prerequisitesText.split()[0]
+    if len(first_word) > 4 or "none." in first_word:
+        return prerequisitesArr
+
+    # get string of only prerequisites
+    end = prerequisitesText.index(".")
+    prerequisitesText = prerequisitesText[:end]
+    prerequisitesText = prerequisitesText.replace('\u2013', '-')
+
+    # separate all prerequisites
+    # ex: [CSE 15L, CSE 20 or MATH 109]
+    prerequisitesArr = prerequisitesText.split(" and ")
+    prerequisitesArr = [req.split(" or ") for req in prerequisitesArr]
+        
+    return prerequisitesArr
+
+# Test
+htmlContent = getHTMLContent('https://catalog.ucsd.edu/courses/CSE.html')
+courseCatalog = getCourseCatalog(htmlContent)
+print(json.dumps(courseCatalog, indent=4))
+
